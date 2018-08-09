33352ee0642a15e6045bf24ec2371df98bb3a395
Lenka Lasonova <lenka.lasonova@seznam.cz>
2018-08-09 16:50:17 +0200

    cyklus

---
+++ cyklus.py
@@ -0,0 +1,4 @@
+print("Ozvěna!") #vypíše Ozvěna!
+slovo = input("Zadejte slovo: ") #získá od uživatele vstup a uloží jej do proměnné
+výsledek = slovo + " " + slovo # vytvoří novou proměnnou
+print(výsledek)
