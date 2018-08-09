aa3e35985a94fe23800086f3b97d689d569691b2
Lenka Lasonova <lenka.lasonova@seznam.cz>
2018-08-09 16:48:01 +0200

    square

---
+++ ctverec.py
@@ -0,0 +1,4 @@
+#Vzorec pro výpočet obvodu a obsahu čtverce
+strana = float(input("Zadej stranu čtverce v centimetrech:"))
+print("Odvod čtverce se stranou", strana, "cm je", 4*strana, "cm")
+print("Obsah čtverce se stranou", strana, "cm je", strana*strana, "cm2")
